d728314fb3a873abd400918dd32885710e7590a4
Jonas Boner <jonas@jonasboner.com>
2009-01-03 16:59:42 +0100

    added publishing scripts

--- bin/publish_blog.sh
+++ bin/publish_blog.sh
@@ -1,6 +1,7 @@
 #! /bin/sh
 MIRROR_SCRIPT='./curlmirror.pl'
 TARGET_SITE='http://jboner.github.com'
+MIRROR='site'
 FTP_SITE='jonasboner.com'
 FTP_USER='jonabon7'
 FTP_PASSWORD='7dc1c1d'
@@ -9,7 +10,9 @@ FTP_SITE_DIR='public_html/jekyll'
 echo '====================================='
 echo 'Grabbing site:' $TARGET_SITE
 echo '===================================================='
-$MIRROR_SCRIPT -v -o . $TARGET_SITE
+$MIRROR_SCRIPT -v -o $MIRROR $TARGET_SITE
+
+cd $MIRROR
 
 echo '===================================================='
 echo 'Uploading site:' $FTP_SITE
@@ -18,6 +21,11 @@ ncftp -u $FTP_USER -p $FTP_PASSWORD $FTP_SITE<<END_SCRIPT
 cd $FTP_SITE_DIR
 put -R .
 quit
+
+echo '===================================================='
+echo 'Cleaning up'
+cd ..
+rm -rf $MIRROR
 
 echo '===================================================='
 echo 'Site mirrored successfully'
