e218e88cd5e1e5acfd5c3e032742bd634a597978
Jonas Boner <jonas@jonasboner.com>
2009-01-09 20:20:23 +0100

    switched to lft and mirror option

--- publish.sh
+++ publish.sh
@@ -1,10 +1,13 @@
 #! /bin/sh
 
+
 SITE='_site'
 FTP_SITE='jonasboner.com'
 FTP_USER='jonabon7'
 FTP_PASSWORD='7dc1c1d'
 FTP_SITE_DIR='public_html'
+
+jekyll
 
 cd $SITE
 rm ./publish.sh
@@ -13,10 +16,11 @@ rm -rf bin
 echo '===================================================='
 echo 'Uploading site:' $FTP_SITE
 echo '===================================================='
-ncftp -u $FTP_USER -p $FTP_PASSWORD $FTP_SITE<<END_SCRIPT
+lftp -d -u $FTP_USER,$FTP_PASSWORD $FTP_SITE<<END_SCRIPT
 cd $FTP_SITE_DIR
-put -R .
-put -R ../../css
+mirror -n -R . /
+#put -R .
+put ../../css
 quit
 
 echo '===================================================='
