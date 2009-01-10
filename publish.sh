30808f029e4daff7e8ff3171cd68d99a6fc13350
Jonas Boner <jonas@jonasboner.com>
2009-01-10 23:21:35 +0100

    new blog post

--- publish.sh
+++ publish.sh
@@ -5,9 +5,6 @@ SITE='_site'
 FTP_SITE='jonasboner.com'
 FTP_USER='jonabon7'
 FTP_PASSWORD='7dc1c1d'
-FTP_SITE_DIR='public_html'
-
-jekyll
 
 cd $SITE
 rm ./publish.sh
@@ -17,9 +14,7 @@ echo '===================================================='
 echo 'Uploading site:' $FTP_SITE
 echo '===================================================='
 lftp -d -u $FTP_USER,$FTP_PASSWORD $FTP_SITE<<END_SCRIPT
-cd $FTP_SITE_DIR
-mirror -n -R . /
-#put -R .
+mirror -n -R . /public_html
 put ../../css
 quit
 
