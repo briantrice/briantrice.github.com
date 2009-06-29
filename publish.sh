9f6cda5c635f6dcd9a435a4dc65660007c087ebf
Jonas Boner <jonas@jonasboner.com>
2009-06-29 07:45:06 +0200

    clean up

--- publish.sh
+++ publish.sh
@@ -1,21 +1,19 @@
 #! /bin/sh
-
-
+ 
 SITE='_site'
 FTP_SITE='jonasboner.com'
-FTP_USER='jonabon7'
-FTP_PASSWORD='7dc1c1d'
-
+ 
 cd $SITE
+echo 'Stepping into' $SITE
 rm ./publish.sh
 rm -rf bin
-
+ 
 echo '===================================================='
 echo 'Uploading site:' $FTP_SITE
 echo '===================================================='
 lftp -d -u $FTP_USER,$FTP_PASSWORD $FTP_SITE<<END_SCRIPT
 mirror -n -R . /public_html
 quit
-
+ 
 echo '===================================================='
 echo 'Site published'
